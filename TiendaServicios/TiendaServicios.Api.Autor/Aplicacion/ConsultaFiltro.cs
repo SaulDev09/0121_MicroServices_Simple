d6d82e5e327c4667f4be0adab17717ae7cc027fd
Saul Chipana <jschipanajara@dimaticasoftware.com>
2021-01-14 17:12:25 +0100

    + Carga inicial

---
+++ TiendaServicios/TiendaServicios.Api.Autor/Aplicacion/ConsultaFiltro.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class ConsultaFiltro
+    {
+        public class AutorUnico : IRequest<AutorDto>
+        {
+            public string AutorLibroGuid { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<AutorUnico, AutorDto>
+        {
+            private readonly ContextoAutor _contexto;
+            private readonly IMapper _mapper;
+            public Manejador(ContextoAutor contexto, IMapper mapper)
+            {
+                _contexto = contexto;
+                _mapper = mapper;
+            }
+
+            public async Task<AutorDto> Handle(AutorUnico request, CancellationToken cancellationToken)
+            {
+                var autor = await _contexto.AutorLibro.Where(x => x.AutorLibroGuid == request.AutorLibroGuid).FirstOrDefaultAsync();
+                if (autor == null) {
+                    throw new Exception("No se encuentra el autor");
+                }
+
+                var autorDto = _mapper.Map<AutorLibro, AutorDto>(autor);
+
+                return autorDto;
+            }
+        }
+
+    }
+}
---
+++ TiendaServicios/TiendaServicios.Api.Libro/Aplicacion/Consulta.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Libro.Model;
+using TiendaServicios.Api.Libro.Persistencia;
+
+namespace TiendaServicios.Api.Libro.Aplicacion
+{
+    public class Consulta
+    {
+        public class ListaLibro : IRequest<List<LibroMaterialDto>>
+        {
+
+        }
+
+        public class Manejador : IRequestHandler<ListaLibro, List<LibroMaterialDto>>
+        {
+            private readonly ContextoLibreria _contexto;
+            private readonly IMapper _mapper;
+
+            public Manejador(ContextoLibreria contexto, IMapper mapper)
+            {
+                _contexto = contexto;
+                _mapper = mapper;
+            }
+
+            public async Task<List<LibroMaterialDto>> Handle(ListaLibro request, CancellationToken cancellationToken)
+            {
+                var libros = await _contexto.LibreriaMaterial.ToListAsync();
+                var librosDto = _mapper.Map<List<LibreriaMaterial>, List<LibroMaterialDto>>(libros);
+                return librosDto;
+            }
+        }
+    }
+}
